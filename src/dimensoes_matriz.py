76125f9a6bc06741fb063f518f6822e2074cd63e
caio ferreria <abreuferr@gmail.com>
2018-11-17 17:00:20 -0200

    dimensoes_matriz.py

---
+++ src/dimensoes_matriz.py
@@ -0,0 +1,20 @@
+# AUTORES    : Caio Abreu Ferreira <abreuferr (a) gmail.com>
+# DESCRIÇÃO  : funcao que define o tamanho de uma matriz
+
+# funcao para definir o tamanho da matriz
+def dimensoes(minha_matriz):
+    numero_linha = len(minha_matriz)
+    numero_coluna = len(minha_matriz[0])
+
+    # retorna os resultados
+    return numero_linha, numero_coluna
+
+# dados a serem verificados
+minha_matriz = [[1, 2, 3], [4, 5, 6]]
+
+# chama a matriz para calcular o numero
+# de linha e coluna
+resultado = dimensoes(minha_matriz)
+
+# exibe o resultado das dimensoes da matriz
+print(resultado)
