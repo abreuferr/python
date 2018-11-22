e7bb928d4467161cf779684f308bb54d1285ad35
caio ferreria <abreuferr@gmail.com>
2018-11-22 15:12:56 -0200

    estudo de oop e python, adicionando novos comentarios

--- src/oop_1.py
+++ src/oop_1.py
@@ -4,15 +4,21 @@
 class carro:
     pass
 
+# duas instancias, meu_carro e seu_carro, do 
+# objeto carro()
 meu_carro = carro()
 seu_carro = carro()
 
+# tres atributos, ano/modelo/cor, e foi adicionado 
+# dados a esses tres atributos, 1968/fusca/azul
 meu_carro.ano = 1968
 meu_carro.modelo = "fusca"
 meu_carro.cor = "azul"
-print(meu_carro.ano)
+print(meu_carro.ano) # 
 print(meu_carro.cor)
 
+# tres atributos, ano/modelo/cor, e foi adicionado 
+# dados a esses tres atributos, 1991/brasilia/amarelo
 seu_carro.ano = 1991
 seu_carro.modelo = "brasilia"
 seu_carro.cor = "amarelo"
