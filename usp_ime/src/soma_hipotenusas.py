88bbb236f5cf7f141c0d410406951ea48e48e389
caio ferreria <abreuferr@gmail.com>
2018-12-26 19:46:51 -0200

    soma_hipotenusas.py

--- usp_ime/src/soma_hipotenusas.py
+++ usp_ime/src/soma_hipotenusas.py
@@ -9,27 +9,53 @@ algum triângulo retângulo com catetos inteiros.
 import math
 
 def soma_hipotenusas(numero):
-    e_hipotenusa(numero)
-
-    return
-
-def e_hipotenusa(numero):
     # inicializando variavel
     contador_a = 1
     contador_b = 1
     contador_c = 1
 
+    # inicializando array
+    array_e_raiz = []
+
+    # varrer todas as possibilidades possivel de se formar um
+    # triangulo cˆ2 = aˆ2 + bˆ2
     while contador_c <= numero:
         while contador_a <= numero:
             while contador_b <= numero:
-                if math.pow(contador_c,2) == math.pow(contador_a,2) + math.pow(contador_b,2):
-                    print(contador_c,"=",contador_a,"+",contador_b)
+                # verifica se os numeros forma um triangulo aristotelico
+                e_raiz = e_hipotenusa(contador_a,contador_b,contador_c)
+                if e_raiz != None:
+                    # armazenar os valores obtidos no array "array_e_raiz"
+                    array_e_raiz.append(e_raiz)
+                
                 contador_b += 1
+            
             contador_b = 1
             contador_a += 1
+
         contador_a = 1
         contador_c += 1
-    return
+
+    # eliminar valores duplicados presentes no array "array_e_raiz"
+    total_e_raiz = []
+    for i in array_e_raiz:
+        if i not in total_e_raiz:
+            total_e_raiz.append(i)
+    
+    # soma de todos os elementros do array "total_e_raiz"
+    total = sum(total_e_raiz)
+    return total
+
+def e_hipotenusa(contador_A, contador_B, contador_C):
+    # inicializa variavel
+    E_raiz = 0
+
+    # vairifica se eh um triangulo pitagorico
+    if math.pow(contador_C,2) == math.pow(contador_A,2) + math.pow(contador_B,2):
+        E_raiz = contador_C
+        return E_raiz # se eh raiz, retorna com o valor
+    
+    return # se nao eh raiz, retorna zero
 
 # main()
 numero = int(input("insira um numero(n) inteiro positivo : "))
