44aae6304328ab4c143be2cf4cb120b75ac978cd
caio ferreria <abreuferr@gmail.com>
2019-05-30 13:43:38 -0300

    estudo sobre variavel numerica

---
+++ livro/src/02_numero.py
@@ -0,0 +1,31 @@
+# AUTORES    : Caio Abreu Ferreira <abreuferr (a) gmail.com>
+# DESCRIÇÃO  : variavel do tipo numero.
+
+#
+# numero
+# 
+# definindo variavel - numero inteiro
+#valor_x = 3
+#valor_y = 9
+#valor_z = 5
+
+# definindo variavel - numero ponto flutuante
+valor_x = 3.2
+valor_y = 9.7
+valor_z = 5.9
+
+# expressao numerica
+total_soma = valor_x + valor_y
+total_subtracao = valor_y - valor_x
+total_multiplicacao = valor_y * valor_x
+total_divisao = valor_y / valor_x
+total_exponenciacao = valor_x ** valor_y
+total_expresssao = (valor_x + valor_z) * valor_y
+
+# imprimindo variavel
+print("total da soma:", total_soma)
+print("total da subtracao:", total_subtracao)
+print("total da multiplicacao:", total_multiplicacao)
+print("total da divisao:", total_divisao)
+print("total da exponenciacao: ", total_exponenciacao)
+print("total da expressao: ", total_expresssao)
