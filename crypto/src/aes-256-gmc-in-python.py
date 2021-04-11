c347cfb385b7bc6f8563eda0dd45cfd39a084d01
caio abreu ferreira <abreuferr@gmail.com>
2021-04-11 10:25:30 -0300

    AES-256-GCM

---
+++ crypto/src/aes-256-gmc-in-python.py
@@ -0,0 +1,41 @@
+# antes de executar o código abaixo, é necessário importar
+# o pacote pycryptodome no python. para isso, basta
+# executar o comando abaixo.
+#
+# pip install pycryptodome
+
+# importando 
+from Crypto.Cipher import AES
+import binascii, os
+
+# funcao utilizada para criptografar os dados
+def encrypt_AES_GCM(msg, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+# funcao utilizada para descriptografar os dados
+def decrypt_AES_GCM(encryptedMsg, secretKey):
+    (ciphertext, nonce, authTag) = encryptedMsg
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
+
+secretKey = os.urandom(32)  # chave de 256-bit gerada de forma ramdomica
+print("Encryption key:", binascii.hexlify(secretKey))
+
+# string que sera critpografada
+msg = b'Message for AES-256-GCM + Scrypt encryption'
+
+# chamada da funcao que ira criptografar a string de ados
+encryptedMsg = encrypt_AES_GCM(msg, secretKey)
+
+print("encryptedMsg", {
+    'ciphertext': binascii.hexlify(encryptedMsg[0]), # string criptografada
+    'aesIV': binascii.hexlify(encryptedMsg[1]), # vetor de inicializacao de 128 bits, 32 hex
+    'authTag': binascii.hexlify(encryptedMsg[2]) # MAC - message authentication code
+})
+
+# chamada da funcao que ira descriptografar a string
+decryptedMsg = decrypt_AES_GCM(encryptedMsg, secretKey)
+print("decryptedMsg", decryptedMsg)
--- crypto/src/aes-ctr-in-python.py
+++ crypto/src/aes-ctr-in-python.py
@@ -10,5 +10,34 @@ import pyaes, pbkdf2, binascii, os, secrets
 # Derivando a chave de criptografia 256-bit AES baseada na senha
 password = "s3cr3t*c0d3" # definindo a senha
 passwordSalt = os.urandom(16) # definindo o valor do salt
-key = pbkdf2.pbkdf2(password, passwordSalt).read(32) # calculando a chave
+key = pbkdf2.PBKDF2(password, passwordSalt).read(32) # calculando a chave
 print('AES encryption key:', binascii.hexlify(key)) # exibindo a chave
+
+# criptografar um texto com uma chave abaixo:
+# ciphertext = AES-256-CTR-Encrypt(plaintext, key, iv)
+
+# vetor de inicializacao
+iv = secrets.randbits(256)
+
+# texto a ser cifrado
+plaintext = "Text for encryption"
+
+# chave de criptografia
+aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
+
+# cifrando o texto
+ciphertext = aes.encrypt(plaintext)
+
+# exibindo o texto cifrado
+print('Encrypted:', binascii.hexlify(ciphertext))
+
+# descriptografa o texto previamente criptografado com a chave
+# plaintext = AES-256-CTR-Decrypt(ciphertext, key, iv)
+
+aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
+
+# descriptografando o dado
+decrypted = aes.decrypt(ciphertext)
+
+# exibindo a mensagem original.
+print('Decrypted:', decrypted)
