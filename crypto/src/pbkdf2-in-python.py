75a559a4f394e85fb714badb723c5e9af67fca8a
caio ferreria <abreuferr@gmail.com>
2021-07-07 15:34:51 -0300

    testes com o algoritmo dfk

--- crypto/src/pbkdf2-in-python.py
+++ crypto/src/pbkdf2-in-python.py
@@ -16,7 +16,7 @@ passwd = "p@$Sw0rD~1".encode("utf8")
 
 # calculando o valor da derivada da chave
 # key = pbkdf2_hmac(hash function, password, salt, interactions, key lenght)
-key = pbkdf2_hmac("sha256", passwd, salt, 5000, 32)
+key = pbkdf2_hmac("sha256", passwd, salt, 100000, 32)
 
 # exibindo a derivada da chave
 print("Derived key:", binascii.hexlify(key))
